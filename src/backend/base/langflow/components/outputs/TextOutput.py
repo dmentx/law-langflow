dcedd325a25e4509c1f14a5ec0d428c02c45f141
Daniel.Mentjukov <daniel.mentjukov@osborneclarke.com>
2024-09-26 15:45:11 +0200

    edited text outp

--- src/backend/base/langflow/components/outputs/TextOutput.py
+++ src/backend/base/langflow/components/outputs/TextOutput.py
@@ -1,4 +1,5 @@
 from langflow.base.io.text import TextComponent
+from langflow.inputs.inputs import StrInput
 from langflow.io import MultilineInput, Output
 from langflow.schema.message import Message
 
@@ -15,14 +16,27 @@ class TextOutputComponent(TextComponent):
             display_name="Text",
             info="Text to be passed as output.",
         ),
+        StrInput(
+            name="text_output_value",
+            display_name= "Text Output",
+            info = "Text to be shown to User",
+            advanced= True
+        )
     ]
     outputs = [
         Output(display_name="Text", name="text", method="text_response"),
     ]
 
     def text_response(self) -> Message:
-        message = Message(
+        if not self.text_output_value:
+             message = Message(
             text=self.input_value,
         )
-        self.status = self.input_value
-        return message
+             self.status = self.input_value
+             return message
+        else:
+            message = Message(
+                text=self.text_output_value
+            )
+            self.status = self.text_output_value
+            return message
